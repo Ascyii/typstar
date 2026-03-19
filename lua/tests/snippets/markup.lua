fce47cdad725b87d1fcad9b834309f5e1c98e039
arne314 <mail@arne.one>
2026-03-20 00:03:37 +0100

    minor: fix test case

--- lua/tests/snippets/markup.lua
+++ lua/tests/snippets/markup.lua
@@ -5,7 +5,7 @@ helper:add_cases('markup', {
     ['logic'] = function() helper:test_snip('IMPIFF', '$==>$ $<==>$ ') end,
     ['math'] = function() helper:test_snip('llAtp\\j a', '$A^top $ a') end,
     ['blockmath'] = function() helper:test_snip('dmpi>e\\ja', '$\n  pi>e\n$ a') end,
-    ['blockmath2'] = function() helper:test_snip('abc dmpi\\ja', 'abc \n$\n  pi\n$ a') end,
+    ['blockmath2'] = function() helper:test_snip('abc dmpi\\ja', 'abc\n$\n  pi\n$ a') end,
     ['blockmath3'] = function() helper:test_snip('  dmpi\\ja', '  $\n    pi\n  $ a') end,
     ['blockmath4'] = function() helper:test_snip('- abc dmpi\\ja', '- abc \n  $\n    pi\n  $ a') end,
 })
