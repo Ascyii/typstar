0f0d2717caa1ff440a548e70e9fa1d2ad7c819d8
arne314 <mail@arne.one>
2026-03-20 00:02:38 +0100

    style: format code

--- lua/tests/test_main.lua
+++ lua/tests/test_main.lua
@@ -5,10 +5,6 @@ return helper:add_cases('main', {
         local jsregexp_ok, _ = pcall(require, 'jsregexp')
         helper.eq(jsregexp_ok, true)
     end,
-    ['toggle_snippets'] = function()
-        helper:test_snip('\\t:a\\t :a', ':a $a$ ')
-    end,
-    ['jump'] = function()
-        helper:test_snip_math('ff1\\j2\\b3\\j\\ja', '(3) / (2) a')
-    end,
+    ['toggle_snippets'] = function() helper:test_snip('\\t:a\\t :a', ':a $a$ ') end,
+    ['jump'] = function() helper:test_snip_math('ff1\\j2\\b3\\j\\ja', '(3) / (2) a') end,
 })
