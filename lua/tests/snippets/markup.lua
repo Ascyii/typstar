71c43137dcc5ff7d0f8db0ae8fe6deb528a255e3
arne314 <mail@arne.one>
2026-03-23 23:46:08 +0100

    test(snip): more visual snippet cases

--- lua/tests/snippets/markup.lua
+++ lua/tests/snippets/markup.lua
@@ -2,6 +2,10 @@ local helper = require('tests.helper'):setup()
 
 helper:add_cases('markup', {
     ['emph'] = function() helper:test_snip('BLDbold\\j ITLitalic', '*bold* _italic_') end,
+    ['selection'] = function()
+        helper:store_selection('hello world')
+        helper:test_snip('HIG', '#highlight[hello world]')
+    end,
     ['logic'] = function() helper:test_snip('IMPIFF', '$==>$ $<==>$ ') end,
     ['math'] = function() helper:test_snip('kkAtp\\j a', '$A^top $ a') end,
     ['blockmath'] = function() helper:test_snip('dmpi>e\\ja', '$\n  pi>e\n$ a') end,
--- lua/tests/snippets/visual.lua
+++ lua/tests/snippets/visual.lua
@@ -16,10 +16,33 @@ helper:add_cases('visual_selection', {
 helper:add_cases('visual_postfix', {
     ['postfix'] = function() helper:test_snip_math('aht', 'hat(a)') end,
     ['long'] = function() helper:test_snip_math('alphaht', 'hat(alpha)') end,
+    ['long2'] = function() helper:test_snip_math('hat(b)alphaht', 'hat(b)hat(alpha)') end,
+    ['long3'] = function()
+        helper:set_buffer('$hat(a)b\\C hat(c)$')
+        helper:test_snip('ht', '$hat(a)hat(b) hat(c)$')
+    end,
     ['nested'] = function() helper:test_snip_math('artht', 'hat(sqrt(a))') end,
-    ['brackets'] = function() helper:test_snip_math('(a)ht', 'hat(a)') end,
+    ['nested2'] = function()
+        helper:set_buffer('$sqrt(abs(a^2\\C) + b^2)$')
+        helper:test_snip('ht', '$sqrt(abs(a^hat(2)) + b^2)$')
+    end,
     ['precedence'] = function() helper:test_snip_math('a_alphaht', 'a_hat(alpha)') end,
     ['precedence2'] = function() helper:test_snip_math('a_b_alphaht', 'a_b_hat(alpha)') end,
+    ['brackets'] = function() helper:test_snip_math('(a)ht', 'hat(a)') end,
+    ['brackets2'] = function() helper:test_snip_math('(a)sq', '[(a)]') end,
+    ['brackets3'] = function() helper:test_snip_math('(a)sQ', '[a]') end,
+    ['brackets4'] = function() helper:test_snip_math('[a]BB', 'a') end,
+    ['brackets5'] = function() helper:test_snip_math('([a])BB', '[a]') end,
+    ['index'] = function() helper:test_snip_math('b_aht', 'b_hat(a)') end,
+    ['index_brackets'] = function() helper:test_snip_math('b_(a)ht', 'b_hat(a)') end,
+    ['multiline'] = function()
+        helper:set_buffer('$sqrt(a\nb)\\C$')
+        helper:test_snip('ht', '$hat(sqrt(a\nb))$')
+    end,
+    ['multiline2'] = function()
+        helper:set_buffer('$(0+sqrt(a^2\n+[b^2])\t\n=c)\\C$')
+        helper:test_snip('ht', '$hat(0+sqrt(a^2\n+[b^2]) \n=c)$')
+    end,
 })
 
 helper:add_cases('visual_normal', {
